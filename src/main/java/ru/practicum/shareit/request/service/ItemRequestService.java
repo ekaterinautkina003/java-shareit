4bfacb5030a85ed0e8b9be0c83a3e896ce0a1ccb
ekaterinautkina003 <katusha_utkina@mail.ru>
2024-05-06 13:35:01 +0300

    2

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request.service;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestShortDto;
@@ -9,11 +8,11 @@ import java.util.List;
 
 public interface ItemRequestService {
 
-  List<ItemRequestDto> getAll(Long userId, Pageable pageable);
+    List<ItemRequestDto> getAll(Long userId, Pageable pageable);
 
-  ItemRequestDto get(Long userId, Long requestId);
+    ItemRequestDto get(Long userId, Long requestId);
 
-  List<ItemRequestDto> getSelf(Long userId);
+    List<ItemRequestDto> getSelf(Long userId);
 
-  ItemRequestDto create(Long usedId, ItemRequestShortDto itemRequestShortDto);
+    ItemRequestDto create(Long usedId, ItemRequestShortDto itemRequestShortDto);
 }
